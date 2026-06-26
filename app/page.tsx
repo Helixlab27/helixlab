79ab8c021b5d431138e809fa7adb3469169c88d8
Helixlab27 <helixlab27@gmail.com>
2026-06-26 13:44:38 +0200

    redirect Home vers /accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation'
 
-export default function RootPage() {
-  redirect('/accueil');
+export default function Home() {
+  redirect('/accueil')
 }
